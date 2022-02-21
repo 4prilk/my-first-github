e35c6f800c5cbf7017ad7231cd8ae68e997ccc7e
koomj <kmj_423@naver.com>
2022-02-21 16:23:38 +0900

    수정2

--- mycode.R
+++ mycode.R
@@ -17,4 +17,64 @@ str(anscombe)
 anscombe[, c("x1","y1")]
 
 df <- anscombe
-plot(df$x1, df$x2, pch=19, col="tomato")
+
+par(mfrow=c(2,2))
+
+plot(df$x1, df$y1, pch=19, col="tomato")
+abline(lm(y1~x1, data=df),col="blue")
+plot(df$x2, df$y2, pch=19, col="tomato")
+abline(lm(y2~x2, data=df),col="blue")
+plot(df$x3, df$y3, pch=19, col="tomato")
+abline(lm(y3~x3, data=df),col="blue")
+plot(df$x4, df$y4, pch=19, col="tomato")
+abline(lm(y4~x4, data=df),col="blue")
+
+par(mfrow=c(1,1)
+    
+    
+# 18 데이터 시각화 실습
+install.packages("tidyverse")
+library(tidyverse)
+str(mpg)
+head(mpg)
+p <- ggplot(data=mpg,
+            mapping=aes(x=displ, y=hwy))
+p # 배경화면이므로 +하여 그래프 갖다붙이기
+
+p+geom_point()
+p+ geom_point(mapping=aes(color=class))
+
+
+head(colors(),10)
+colors()
+
+
+p+ geom_point(mapping=aes(color=class, size=class,
+                          shape=class, alpha=0.3))
+
+p+geom_point(color="tomato") +
+  facet_wrap(~class, nrow=2)
+
+p+geom_point(color="orange") +
+  facet_grid(drv~cyl)
+
+
+# 
+table(mpg$drv)
+?mpg
+table(mpg$cyl) # 실린더 개수
+par(mfrow=c(1,1))
+barplot(table(mpg$cyl), col="steelblue")
+
+
+p + geom_point(color = "steelblue") +
+  geom_smooth(color="tomato") # 값에 대한 추세선을 그림림
+
+
+p+ geom_point(
+    mapping=aes(color=class))+
+  geom_smooth(color="tomato")
+
+
+data("diamonds")
+str(diamonds)
