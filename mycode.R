c20ec421bf03989b1e7f3027d81f17a20b09e58c
koomj <kmj_423@naver.com>
2022-02-21 17:58:47 +0900

    2/21 수정

--- mycode.R
+++ mycode.R
@@ -78,3 +78,65 @@ p+ geom_point(
 
 data("diamonds")
 str(diamonds)
+
+
+
+p <- ggplot(data = diamonds)
+p + geom_bar(mapping = aes(x = cut),
+           fill = "steelblue")
+p + stat_count(mapping = aes(x = cut),
+               fill = "steelblue")
+
+
+
+p + geom_bar(mapping = aes(x = cut,
+                           fill = clarity))
+             
+p + geom_bar(mapping = aes(x = cut,
+                           fill = clarity), # 색을 클래스로 줌
+             position = "fill")  # 막대를 전부 채움
+p + geom_bar(mapping = aes(x = cut,
+                           fill = clarity),
+             position = "dodge")
+
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ,
+                           y = hwy,
+                           color = class),
+             position = "jitter") # 산점도
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ,
+                 y = hwy,
+                 color = class,
+                 size = class),
+             position = "jitter")
+
+
+ # 박스플롯
+p <- ggplot(data = mpg,
+            mapping = aes(x=class, y=hwy))
+p + geom_boxplot(fill = "lightyellow")
+
+p + geom_boxplot(fill = "lightyellow") +
+  coord_flip()
+
+
+p <- ggplot(data = diamonds)
+p + geom_bar(mapping = aes(x = cut,
+                           fill = clarity),position = "fill") +
+  coord_flip()
+
+
+p <- ggplot(data = diamonds,
+            mapping = aes(x=cut, fill=cut))
+p + geom_bar(show.legend=F,width=1) +
+  labs(x=NULL,y=NULL) +
+  theme(aspect.ratio=1) +
+  coord_polar() # 극좌표계
+
+
+world <- map_data("world")
+ggplot(world, aes(long, lat, group = group)) +
+  geom_polygon(fill = "lightyellow", color="blue")
